4df2adccd7b496117e53d5bc4204618488d3b22a
CMcQuay <calebimcquay@gmail.com>
2023-10-10 23:28:21 -0500

    All tasks complete

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -3,8 +3,13 @@ package com.ualr.resources;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -14,10 +19,26 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView countryDescriptionTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String systemLanguage = Resources.getSystem().getConfiguration().getLocales().get(0).getLanguage();
+
+        countryDescriptionTextView = findViewById(R.id.country_description_text);
+        countryDescriptionTextView.setText(getString(R.string.country_description));
+
+        Button button = findViewById(R.id.lesson_learnt_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a Toast message when the button is clicked
+                Toast.makeText(MainActivity.this, R.string.congrats_text, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 
 }
